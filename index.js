81a5aba799153bf1f2495476445bf405f539165b
Xavier Loup <loupxavi@amazon.com>
2019-08-09 15:57:52 +0000

    Remove context succeed

--- index.js
+++ index.js
@@ -38,17 +38,16 @@ exports.handler =  async function(event, context) {
     : "Sorry, I don't understand";
   console.log("EVENT: \n" + JSON.stringify(event, null, 2))
   
-    var responseCode = 200;
+  var response = {
+    statusCode: 200,
+    headers: {
+      "Access-Control-Allow-Origin": "*"
+    },
+    body: {
+      message: JSON.stringify(answer),
+      context: JSON.stringify(conversationContext)
+    }
+  };
     
-    var response = {
-        statusCode: responseCode,
-        headers: {
-            "Access-Control-Allow-Origin": "*"
-        },
-        body: { message: JSON.stringify(answer), context: JSON.stringify(conversationContext) }
-    };
-    
-    context.succeed(response);
-
   return response;
 }

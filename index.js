d6e858745ac26aaef5c5c56e6eee7c31b2544ec4
Xavier Loup <loupxavi@amazon.com>
2019-08-09 15:04:22 +0000

    new context mgmt

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ nlpManager.load('./model.nlp');
 exports.handler =  async function(event, context) {
   console.log(event);
   
-  let result = await nlpManager.process('fr', event.body.message, conversationContext);
+  let result = await nlpManager.process('fr', JSON.parse(event.body).message, conversationContext);
   let answer =
     result.score > threshold && result.answer
     ? result.answer
